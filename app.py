5e361d375fa7893522f9505b0eb9a01e8dcdab71
algadoc <alfonsoldt2003@gmail.com>
2021-10-23 20:10:33 -0400

    Addded ML model & form processing

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-import pickle
+import pickle   
+import joblib
+
 
 app = Flask(__name__)
-model = pickle.load(open('RFModel.p', 'rb'))
+model = joblib.load('model.pkl')
+encoder = joblib.load('enc_joblib.pkl')
 
 @app.route('/')
 def home():
@@ -14,11 +17,15 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    int_features = [x for x in request.form.values()]
-    print(int_features)
-    final_features = [np.array(int_features)]
+    features = [x for x in request.form.values()]
+    print(features)
+    integer_features = np.array([int(x) for x in features[:-2]])
+    print([features[-2:]])
+    onehots = encoder.transform(np.array(features[-2:]).reshape(-1,2)).toarray()
+    print(onehots)
+    print(onehots.shape)
+    final_features = np.append(integer_features, onehots).reshape(-1,1)
     prediction = model.predict(final_features)
-
     output = round(prediction[0], 2)
 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
